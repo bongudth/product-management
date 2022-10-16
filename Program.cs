7c156323dbc4ffae41ec36deb75ad35aa9507ec1
Linh Huynh Thi Khanh <linhhtk@nal.vn>
2022-10-16 23:01:47 +0700

    connect db in

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using demo.Models;
+using demo.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var connectionString = builder.Configuration.GetConnectionString("Default");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+services.AddDbContext<DataContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+services.AddTransient<IProductService, ProductService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
